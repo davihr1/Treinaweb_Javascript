982aa4cb9ec13323a4ceae2eabc138bf548847c6
davihr1 <davihenriquefavaro@gmail.com>
2022-11-10 18:41:32 -0300

    modulo_02

--- aula01/scripts.js
+++ aula01/scripts.js
@@ -1,3 +1,25 @@
-document.querySelector('h1').innerText = 'Rocketseat';
+console.log(5 * 9);
 
-console.log('Treinaweb Javascript');
+// Valores Boolean
+let name = 'davi'
+let name2 = 'vitror'
+
+name == name2 ? console.log('sim') : console.log('nao')
+
+// undefined e null
+// valor indefinido
+name = undefined;
+
+// valor invalido
+name2 = null;
+
+
+
+console.log(3 > 8 ? 'É maior' : 'É menor')
+
+// console.log('Teste  ' + true || false && true && false || true);
+// console.log(true || false);
+// console.log(true || false);
+// console.log(!ture && true);
+// console.log(7 == "7");
+console.log(5 !== 4);
